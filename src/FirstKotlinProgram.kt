691f42c06035541bad37645bbe93ac30e9b58fe5
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2020-10-07 22:32:30 +0500

    First Kotlin Program
    initial commit after 26 days

---
+++ src/FirstKotlinProgram.kt
@@ -0,0 +1,10 @@
+
+
+//First Kotlin Program
+
+//Kotlin is an objetc oriented and a general purpose language that is mainly used for
+//android development
+
+fun main(args: Array<String>){
+    println("Hello World!!")
+}
